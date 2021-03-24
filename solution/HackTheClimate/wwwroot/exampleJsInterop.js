52b603dcc406f3a2244ae4b7ddf91b1886e35915
Christian Abegg <christian.abegg@zuehlke.com>
2021-03-24 09:37:36 +0100

    call js

---
+++ solution/HackTheClimate/wwwroot/exampleJsInterop.js
@@ -0,0 +1,7 @@
+﻿window.convertArray = (win1251Array) => {
+    var win1251decoder = new TextDecoder('windows-1251');
+    var bytes = new Uint8Array(win1251Array);
+    var decodedArray = win1251decoder.decode(bytes);
+    console.log(decodedArray);
+    return decodedArray;
+};
