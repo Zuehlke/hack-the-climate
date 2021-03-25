078f6d98f43cc3e34288a98f5ddbfb106d65c91e
Christian Abegg <christian.abegg@zuehlke.com>
2021-03-25 09:22:45 +0100

    force layout

--- solution/HackTheClimate/wwwroot/diagram.js
+++ solution/HackTheClimate/wwwroot/diagram.js
@@ -1,7 +1,6 @@
 ﻿export async function renderDiagram(element, data) {
     const height = 350;
     const width = 600;
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
 
     const smallestConfidenceScore = data.nodes.map(n => n.confidenceScore).reduce((a, b) => Math.min(a, b));
     const largestConfidenceScore = data.nodes.map(n => n.confidenceScore).reduce((a, b) => Math.max(a, b));
@@ -9,57 +8,74 @@
         .domain([smallestConfidenceScore, largestConfidenceScore])
         .range([3, 10]);
 
-    const index = new Map(data.nodes.map(d => [d.id, d]));
-    const links = data.links.map(d => Object.assign(Object.create(d), {
-        source: index.get(d.source),
-        target: index.get(d.target)
-    }));
+    const simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.id; }))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
 
     const svg = d3.select(element);
     svg.selectAll("*").remove();
-
-    const layout = cola.d3adaptor(d3)
-        .size([width, height])
-        .nodes(data.nodes)
-        .links(links)
-        .jaccardLinkLengths(40, 0.7)
-        .start(30);
 
     const link = svg.append("g")
         .attr("stroke", "#999")
         .attr("stroke-opacity", 0.6)
         .selectAll("line")
-        .data(links)
+        .data(data.links)
         .enter().append("line")
         .attr("stroke-width", d => Math.sqrt(d.value));
 
     const node = svg.append("g")
         .attr("stroke", "#fff")
         .attr("stroke-width", 1.5)
-        .selectAll("circle")
+        .selectAll("g")
         .data(data.nodes)
-        .enter().append("circle")
+        .enter().append("g")
+
+    const circles = node.append("circle")
         .attr("r", d => radiusScale(d.confidenceScore))
         .attr("fill", "#3a0647")
-        .call(layout.drag);
+        .call(d3.drag()
+            .on("start", d => dragstarted(d, simulation))
+            .on("drag", d => dragged(d))
+            .on("end", d => dragended(d, simulation)));
 
     node.append("title")
         .text(d => d.title);
 
-    layout.on("tick", () => {
+    simulation
+        .nodes(data.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(data.links);
+
+    function ticked() {
         link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
 
         node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-    });
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+    }
+}
 
-    // TODO invalidation is undefined
-    // invalidation.then(() => layout.stop());
+function dragstarted(d, simulation) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
 
-    return svg.node();
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d, simulation) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
 }
